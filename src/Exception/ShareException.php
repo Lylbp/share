e7ac3424e6e1aeaa5b6242f91f53b0ce1f759400
alex <weiwenbin@yayuanzi.com>
2019-05-23 11:00:59 +0800

    feat

---
+++ src/Exception/ShareException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 2019/5/23
+ * Time: 上午9:53
+ */
+
+namespace share\Exception;
+
+
+use thl\exception\ThlException;
+
+class ShareException extends ThlException
+{
+
+}
---
+++ src/share/weChat/interfaces/WeChatShareInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 2019/4/28
+ * Time: 下午4:35
+ */
+
+namespace share\share\weChat\interfaces;
+
+
+
+use share\share\interfaces\ShareInterface;
+
+interface WeChatShareInterface extends ShareInterface
+{
+
+}
